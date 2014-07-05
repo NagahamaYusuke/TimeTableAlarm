ced3796c77abd752a64c895db8baff8848a7cb0f
NagahamaYusuke <yusu.aqua.10@gmail.com>
2014-07-06 00:21:43 +0900

    View作成

---
+++ src/com/timetablealarm/alarm/GraphView.java
@@ -0,0 +1,16 @@
+package com.timetablealarm.alarm;
+
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.AbstractChart;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class GraphView extends GraphicalView {
+
+	public GraphView(Context arg0, AbstractChart arg1) {
+		super(arg0, arg1);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
